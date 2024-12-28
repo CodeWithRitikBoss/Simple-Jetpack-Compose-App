410da86cdf8b43ae4d554b05dfff217c35e04d77
Ritik verma <132076091+CodeWithRitikCoder@users.noreply.github.com>
2024-12-29 01:04:21 +0530

    Code Commited.

---
+++ app/src/main/java/com/ritikcoder/simplejetpackcomposeapp/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.ritikcoder.simplejetpackcomposeapp
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.ritikcoder.simplejetpackcomposeapp.ui.theme.SimpleJetpackComposeAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Column {
+                Text(text = "Hello World.")
+                Text(text = "Hello Android.")
+                Button(onClick = {
+                    //onClick
+                    Toast.makeText(applicationContext, "Button is working", Toast.LENGTH_SHORT).show()
+                }) {
+                    Text(text = "Click me")
+                }
+            }
+        }
+    }
+}
+
+
+// TextView, EditText, Button, ImageView.
+// Text, TextField, Button, Image.
